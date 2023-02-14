d81633b1246ce7888493afda54752601ddac87f2
Alexis Peralta <alexisdnlz51@gmail.com>
2023-02-14 12:41:46 -0600

    Validacion Formulario

--- validacion.js
+++ validacion.js
@@ -1,39 +1,62 @@
-const datos = {
+const datos = { //OBJETOS
     rfc: ''
 }
 
-const rfc = document.querySelector('#rfc');
-const formulario = document.querySelector('.formulario');
+// const datos3 = {
+//     regimen : {
+//         fisica : "RESICO",
+//         moral : "Ley general de personal morales"
+//     }
+// }
 
-rfc.addEventListener('input', LeerTexto);
+// console.log(datos3.regimen.fisica)
+// console.log(datos3['regimen'].fisica)
+// const {regimen} = datos3
+// console.log(regimen['fisica'])
+
+
+//EVENTOS
+const rfc = document.querySelector('#rfc')
+const formulario = document.querySelector('.formulario')
+
+
+rfc.addEventListener('input', LeerTexto)
+formulario.addEventListener('submit', Evento);
 
 //EVENTO SUBMIT
-formulario.addEventListener('submit', function(evento){
-    evento.preventDefault();
-
-
-    //VALIDACION
-    const {rfc} = datos;
-
-if (rfc === '') {
-    MostrarError('Esta vacio');
-
-    return;
-}
-
-    console.log('Enviado');
-
-});
 
 function LeerTexto(e){
 
-    datos[e.target.id] = e.target.value;
+    datos[e.target.id] = e.target.value
 
+}
+
+function Evento(evento){
+        evento.preventDefault()
+        //VALIDACION
+        const {rfc} = datos
+        // const datos1 = datos.rfc
+        // const datos2 = datos['rfc']
+        if (datos === '') {
+            MostrarError('Esta vacio')
+        }
+
+        console.log('Se enviara tú nueva contraseña a: ' + rfc)
 }
 
 function MostrarError(mensaje){
     const error = document.createElement('P');
     error.textContent = mensaje;
+    error.classList.add('error')
 
-    console.log();
+    formulario.appendChild(error);
 }
+
+
+// function MostrarEnvio(alerta){
+//     const envio = document.createElement('P');
+//     error.textContent = alerta;
+//     error.classList.add('envio')
+
+//     formulario.appendChild(envio);
+// }
